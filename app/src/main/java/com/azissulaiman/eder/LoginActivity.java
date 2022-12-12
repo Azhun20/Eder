b2405f614ad5039694c0de1e1ef5473e970aa3c0
ersandi15 <ersandibillah03@gmail.com>
2022-12-12 20:18:26 +0700

    push update tampilan 1.2

--- app/src/main/java/com/azissulaiman/eder/LoginActivity.java
+++ app/src/main/java/com/azissulaiman/eder/LoginActivity.java
@@ -1,14 +1,93 @@
 package com.azissulaiman.eder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+import com.azissulaiman.eder.databinding.ActivityLoginBinding;
+import com.azissulaiman.eder.fragments.EmailLoginFragment;
+import com.azissulaiman.eder.fragments.PhoneLoginFragment;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
+    ViewPager2 viewPager2;
+    TabLayout tabLayout;
+    LoginActivity activity;
+    ViewPagerAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        viewPager2 = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+        activity = this;
+        
+        initView();
+
+    }
+
+    private void initView() {
+        adapter = new ViewPagerAdapter(activity.getSupportFragmentManager(),getLifecycle());
+        adapter.addFragment(new EmailLoginFragment(), "Email");
+        adapter.addFragment(new PhoneLoginFragment(), "Phone");
+        viewPager2.setAdapter(adapter);
+        viewPager2.setOffscreenPageLimit(1);
+
+        new TabLayoutMediator(tabLayout, viewPager2,
+                (tab, position)-> {
+                    tab.setText(adapter.mFragmentTitleList.get(position));
+                }).attach();
+
+        for (int i = 0; i<tabLayout.getTabCount(); i++){
+            TextView textView = (TextView) LayoutInflater.from(activity).inflate(R.layout.custom_tab,null);
+            tabLayout.getTabAt(i).setCustomView(textView);
+        }
+        tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#171212"));
+        tabLayout.setTabTextColors(ColorStateList.valueOf(Color.parseColor("#2F4F4F")));
+    }
+
+    class ViewPagerAdapter extends FragmentStateAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NotNull Lifecycle lifecycle) {
+            super(fragmentManager, lifecycle);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @NotNull
+        @Override
+        public Fragment createFragment(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mFragmentList.size();
+        }
     }
 }
--- app/src/main/java/com/azissulaiman/eder/MainActivity.java
+++ app/src/main/java/com/azissulaiman/eder/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn1, btnQrCode;
+    Button btn1, btnQrCode, btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btn1 = findViewById(R.id.btnmap);
         btnQrCode = findViewById(R.id.btnTakeQr);
+        btnLogin = findViewById(R.id.btnLogin1);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +31,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, TakeQRCode.class));
             }
         });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+ //       email.setAlpha(v);
+//        pass.setAlpha(v);
+//        btnLoginEmail.setAlpha(v);
+//        clickSignUpEmail.setAlpha(v);
+//        clickForgotPass.setAlpha(v);
+//
+//        email.animate().translationY(0).alpha(1).setDuration(800).setStartDelay(300).start();
+//        pass.animate().translationY(0).alpha(1).setDuration(800).setStartDelay(500).start();
+//        btnLoginEmail.animate().translationY(0).alpha(1).setDuration(800).setStartDelay(300).start();
+//        clickSignUpEmail.animate().translationY(0).alpha(1).setDuration(800).setStartDelay(300).start();
+//        clickForgotPass.animate().translationY(0).alpha(1).setDuration(800).setStartDelay(500).start();
 
     }
 }
