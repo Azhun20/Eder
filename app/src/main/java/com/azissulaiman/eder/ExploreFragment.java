2a057dd937d691d9ad5c2113a383acb2ca432fc2
BlackBrocolli <201111011@mhs.stiki.ac.id>
2022-12-20 13:40:34 +0700

    Menambahkan horizontal scroll pada fragment explore

--- app/src/main/java/com/azissulaiman/eder/ExploreFragment.java
+++ app/src/main/java/com/azissulaiman/eder/ExploreFragment.java
@@ -1,12 +1,18 @@
 package com.azissulaiman.eder;
 
+import static com.mapbox.mapboxsdk.Mapbox.getApplicationContext;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +29,12 @@ public class ExploreFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private RecyclerView recyclerView;
+    private Adapter adapter;
+    private ArrayList<Buku> bukuArrayList;
+
+    LinearLayoutManager HorizontalLayout;
 
     public ExploreFragment() {
         // Required empty public constructor
@@ -59,6 +71,37 @@ public class ExploreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false);
+        View v = inflater.inflate(R.layout.fragment_explore, container, false);
+
+        addData();
+
+        recyclerView = (RecyclerView) v.findViewById(R.id.recyclerview);
+
+        adapter = new Adapter(bukuArrayList);
+
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+
+        // Set Horizontal Layout Manager
+        // for Recycler view
+        HorizontalLayout
+                = new LinearLayoutManager(
+                this.getContext(),
+                LinearLayoutManager.HORIZONTAL,
+                false);
+
+//        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setLayoutManager(HorizontalLayout);
+
+        recyclerView.setAdapter(adapter);
+
+        return v;
+    }
+
+    void addData() {
+        bukuArrayList = new ArrayList<>();
+        bukuArrayList.add(new Buku(R.drawable.superjitu_cover,"Superjitu Lolos Tes CPNS", "Tim B First", "4.3"));
+        bukuArrayList.add(new Buku(R.drawable.superjitu_cover,"Superjitu Lolos Tes CPNS", "Tim B First", "4.3"));
+        bukuArrayList.add(new Buku(R.drawable.superjitu_cover,"Superjitu Lolos Tes CPNS", "Tim B First", "4.3"));
+        bukuArrayList.add(new Buku(R.drawable.superjitu_cover,"Superjitu Lolos Tes CPNS", "Tim B First", "4.3"));
     }
 }
