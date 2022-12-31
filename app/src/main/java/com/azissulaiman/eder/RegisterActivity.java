63fab420bc63204ce05d1b82b5a81e6cbf4c044e
ersandi15 <ersandibillah03@gmail.com>
2022-12-31 12:48:25 +0700

    Menghilangan Fragment Register

--- app/src/main/java/com/azissulaiman/eder/RegisterActivity.java
+++ app/src/main/java/com/azissulaiman/eder/RegisterActivity.java
@@ -1,91 +1,30 @@
 package com.azissulaiman.eder;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.Lifecycle;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
 
-import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.azissulaiman.eder.fragments.EmailLoginFragment;
-import com.azissulaiman.eder.fragments.EmailRegisterFragment;
-import com.azissulaiman.eder.fragments.PhoneLoginFragment;
-import com.azissulaiman.eder.fragments.PhoneRegisterFragment;
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class RegisterActivity extends AppCompatActivity {
-    ViewPager2 viewPager2;
-    TabLayout tabLayout2;
-    RegisterActivity activity;
-    ViewPagerAdapter adapter;
+
+    Button btnSignUp;
+    TextView ClickIntentSignIn;
+    EditText edtRegUsername, edtRegEmail, edtRegPhone, edtRegPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        viewPager2 = findViewById(R.id.viewPagerReg);
-        tabLayout2 = findViewById(R.id.tabLayoutReg);
-        activity = this;
 
-        initView();
-    }
+        edtRegUsername = (EditText) findViewById(R.id.edtUsernameReg) ;
+        edtRegEmail = (EditText) findViewById(R.id.edtEmailReg);
+        edtRegPhone = (EditText) findViewById(R.id.edtPhoneReg);
+        edtRegPass = (EditText) findViewById(R.id.edtPassReg);
 
-    private void initView() {
-        adapter = new ViewPagerAdapter(activity.getSupportFragmentManager(),getLifecycle());
-        adapter.addFragment(new EmailRegisterFragment(), "Email");
-        adapter.addFragment(new PhoneRegisterFragment(), "Phone");
-        viewPager2.setAdapter(adapter);
-        viewPager2.setOffscreenPageLimit(1);
+        btnSignUp = (Button) findViewById(R.id.btn_signUp_email);
+        ClickIntentSignIn = (TextView) findViewById(R.id.click_signInEmail);
 
-        new TabLayoutMediator(tabLayout2, viewPager2,
-                (tab, position)-> {
-                    tab.setText(adapter.mFragmentTitleList.get(position));
-                }).attach();
-
-        for (int i = 0; i<tabLayout2.getTabCount(); i++){
-            TextView textView = (TextView) LayoutInflater.from(activity).inflate(R.layout.custom_tab_login,null);
-            tabLayout2.getTabAt(i).setCustomView(textView);
-        }
-
-    }
-
-    private class ViewPagerAdapter extends FragmentStateAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        public ViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NotNull Lifecycle lifecycle) {
-            super(fragmentManager, lifecycle);
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @NotNull
-        @Override
-        public Fragment createFragment(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getItemCount() {
-            return mFragmentList.size();
-        }
     }
 }
